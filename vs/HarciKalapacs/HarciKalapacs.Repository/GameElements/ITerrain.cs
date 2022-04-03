110fce4307f33565f24f0b7b7879fb2ce2daa6e1
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2022-04-03 18:36:51 +0200

    Name and form Change

---
+++ vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/ITerrain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarciKalapacs.Repository.GameElements
+{
+    interface ITerrain : IMapItem
+    {
+        bool isOverWalkable{ get; set; }
+        bool isSeeThrough { get; set; }
+        bool isOccupiable { get; set; }
+
+        public bool IsOverWalkable { get => isOverWalkable; set => isOverWalkable = value; }
+        public bool IsSeeThrough { get => isSeeThrough; set => isSeeThrough = value; }
+        public bool IisSeeThrough { get => isSeeThrough; set => isSeeThrough = value; }
+
+    }
+}
