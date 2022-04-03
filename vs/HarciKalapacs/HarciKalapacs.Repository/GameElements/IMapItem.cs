63bd4a7b9a428d12c52de04674ec54ff9e8f9cc2
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2022-04-03 16:08:46 +0200

    New Base Inteface for map Items

---
+++ vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/IMapItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarciKalapacs.Repository.GameElements
+{
+    public interface IMapItem
+    {
+        public int health { get; set; }
+        public int MaxHealth { get; set; }
+        public int YPos { get; set ; }
+        public int XPos { get; set; }
+        public bool IsDestructable { get; set; }
+
+        //override ToHash();
+        //BulidFromHash(Hash);
+    }
+}
