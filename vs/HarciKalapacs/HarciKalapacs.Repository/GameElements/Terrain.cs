bbad86dd0f92d5116e1461042c713460f0f723a7
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2022-04-03 16:10:30 +0200

    Base Interface for Terrain Objects

---
+++ vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/Terrain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarciKalapacs.Repository.GameElements
+{
+    interface Terrain : IMapItem
+    {
+        bool IsOverWalkable{ get; set; }
+        bool IsSeeThrough { get; set; }
+        bool IsOccupiable { get; set; }
+    }
+}
