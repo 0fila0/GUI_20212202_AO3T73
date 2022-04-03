4655e15de1cf7e634456e2eb75ddb5497af20d1a
ÁdámBálint <adambalint9@stud.uni-obuda.hu>
2022-04-03 18:40:29 +0200

    Fixd from and implemented new unit structure

--- vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/IMapItem.cs
+++ vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/IMapItem.cs
@@ -8,12 +8,22 @@ namespace HarciKalapacs.Repository.GameElements
 {
     public interface IMapItem
     {
-        public int health { get; set; }
-        public int MaxHealth { get; set; }
-        public int YPos { get; set ; }
-        public int XPos { get; set; }
-        public bool IsDestructable { get; set; }
+        int hp { get; set; }
+        int maxHp { get; set; }
+        int yPos { get; set; }
+        int xPos { get; set; }
+        bool isDestructable { get; set; }
+        string idleImage { get; set; }
+        string dyingImage { get; set; }
 
+
+        public int Hp { get=>hp; set => hp = value; }
+        public int MaxHp { get => maxHp; set => maxHp = value; }
+        public int YPos { get => yPos; set => maxHp = yPos; }
+        public int XPos { get => xPos; set => xPos = value; }
+        public bool IsDestructable { get => isDestructable; set => isDestructable = value; }
+        public string IdleImage { get => idleImage; set => idleImage = value; }
+        public string DyingImage { get => dyingImage; set => dyingImage = value; }
         //override ToHash();
         //BulidFromHash(Hash);
     }
--- vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/Units.cs
+++ vs/HarciKalapacs/HarciKalapacs.Repository/GameElements/Units.cs
@@ -8,57 +8,78 @@ namespace HarciKalapacs.Repository.GameElements
         player = 1, enemy = 2, natural = 3
     }
 
-    public abstract class Units
+    public interface Units:IMapItem
     {
-        int maxHp;
-        int hp;
-        int xPos;
-        int yPos;
-        Team team;
-        string idleImage1;
-        string dyingImage;
+        /* int maxHp;
+         int hp;
+         int xPos;
+         int yPos;*/
+        Team team { get; set; }
+        int movement { get; set; }
+        bool canSeeTrhoughAll { get; set; }
+        bool canAttack { get; set; }
+        bool canHeal { get; set; }
+        int attackValue { get; set; }
+        int healValue { get; set; }
+        int armorValue { get; set; }
+        bool canGetIntoBuilding { get; set; }
 
-        public int MaxHp { get => maxHp; set => maxHp = value; }
+        public void GetIntoBuilding();
+        public void GetOutOfBuilding();
+
+        public Team Team { get => team; set => team = value; }
+        public int Movement { get => movement; set => movement = value; }
+        public bool CanSeeTroughAll { get => canSeeTrhoughAll; set => canSeeTrhoughAll = value; }
+        public bool CanAttack { get => canAttack; set => canAttack = value; }
+        public bool CanHeal { get => canHeal; set => canHeal = value; }
+        public int AttackValue { get => attackValue; set => attackValue = value; }
+        public int HealValue { get => healValue; set => healValue = value; }
+        public int ArmorValue { get => armorValue; set => armorValue = value; }
+        public bool CanGetIntoBuilding { get => canGetIntoBuilding; set => canGetIntoBuilding = value; }
+        /* string idleImage1;
+         string dyingImage;*/
+
+        /*public int MaxHp { get => maxHp; set => maxHp = value; }
         public int Hp { get => hp; set => hp = value; }
         public int YPos { get => xPos; set => xPos = value; }
         public int XPos { get => yPos; set => yPos = value; }
-        public string DyingImage { get => dyingImage; set => dyingImage = value; }
-        public Team Team { get => team; set => team = value; }
-        public string IdleImage1
-        {
-            get => idleImage1;
-            set
-            {
-                string type = this.GetType().Name;
-                if (this.team == Team.player)
-                {
-                    this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Player\player" + type + "Idle1.png";
-                }
-                else if (this.team == Team.enemy)
-                {
-                    this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Enemy\enemy" + type + "Idle1.png";
-                }
-                else
-                {   
-                    // Let's be an obstacle.
-                    if (this.maxHp == UnitsConfig.Natural.Cover.FenceConfig.MaxHp)
-                    {
-                        this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Cover\fence" + type + "Idle1.png";
-                    }
-                    else if (this.maxHp == UnitsConfig.Natural.Cover.TreeConfig.MaxHp)
-                    {
-                        this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Cover\tree" + type + "Idle1.png";
-                    }
-                    else if (this.maxHp == UnitsConfig.Natural.Obstacle.HouseConfig.MaxHp)
-                    {
-                        this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Obstacle\house" + type + "Idle1.png";
-                    }
-                    else
-                    {
-                        this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Obstacle\mountain" + type + "Idle1.png";
-                    }
-                }
-            }
-        }
+        public string DyingImage { get => dyingImage; set => dyingImage = value; }*/
+
+        /* public string IdleImage1
+         {
+             get => idleImage1;
+             set
+             {
+                 string type = this.GetType().Name;
+                 if (this.team == Team.player)
+                 {
+                     this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Player\player" + type + "Idle1.png";
+                 }
+                 else if (this.team == Team.enemy)
+                 {
+                     this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Enemy\enemy" + type + "Idle1.png";
+                 }
+                 else
+                 {   
+                     // Let's be an obstacle.
+                     if (this.maxHp == UnitsConfig.Natural.Cover.FenceConfig.MaxHp)
+                     {
+                         this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Cover\fence" + type + "Idle1.png";
+                     }
+                     else if (this.maxHp == UnitsConfig.Natural.Cover.TreeConfig.MaxHp)
+                     {
+                         this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Cover\tree" + type + "Idle1.png";
+                     }
+                     else if (this.maxHp == UnitsConfig.Natural.Obstacle.HouseConfig.MaxHp)
+                     {
+                         this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Obstacle\house" + type + "Idle1.png";
+                     }
+                     else
+                     {
+                         this.idleImage1 = Directory.GetCurrentDirectory() + @"\Images" + @"\Units" + @"\Natural\Obstacle\mountain" + type + "Idle1.png";
+                     }
+                 }
+             }
+         }*/
     }
 }
